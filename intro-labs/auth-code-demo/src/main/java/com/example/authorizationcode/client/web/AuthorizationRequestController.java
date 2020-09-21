d3630817dca6f03b43a312e2683e5acbbfcef4b9
luechtdiode <roland.seidel@interpolar.ch>
2020-09-21 13:22:18 +0200

    add github and gitlab prividers

--- intro-labs/auth-code-demo/src/main/java/com/example/authorizationcode/client/web/AuthorizationRequestController.java
+++ intro-labs/auth-code-demo/src/main/java/com/example/authorizationcode/client/web/AuthorizationRequestController.java
@@ -8,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URL;
