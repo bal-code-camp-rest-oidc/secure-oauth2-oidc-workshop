bb8a6662223d8ba6cbe55bfd50689d67eac9b8d1
luechtdiode <roland.seidel@interpolar.ch>
2020-09-22 15:52:19 +0200

    lab 3 complete

--- lab3/library-client-credentials-initial/src/main/java/com/example/library/client/credentials/config/WebClientConfiguration.java
+++ lab3/library-client-credentials-initial/src/main/java/com/example/library/client/credentials/config/WebClientConfiguration.java
@@ -3,6 +3,15 @@ package com.example.library.client.credentials.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Profile("!testing")
@@ -10,7 +19,30 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfiguration {
 
   @Bean
-  WebClient webClient() {
-    return WebClient.builder().build();
+  WebClient webClient(ReactiveClientRegistrationRepository clientRegistrations, ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+    ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
+            new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+                    new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrations, authorizedClientService));
+    oauth.setDefaultClientRegistrationId("library_client");
+    return WebClient.builder()
+            .filter(oauth)
+            .build();
+  }
+
+  @Bean
+  ReactiveClientRegistrationRepository clientRegistrations() {
+    ClientRegistration clientRegistration = ClientRegistrations
+            .fromOidcIssuerLocation("http://localhost:8080/auth/realms/workshop")
+            .registrationId("library_client")
+            .clientId("library-client")
+            .clientSecret("79c14439-c025-487f-8a9d-0060598cb8a3")
+            .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+            .build();
+    return new InMemoryReactiveClientRegistrationRepository(clientRegistration);
+  }
+
+  @Bean
+  ReactiveOAuth2AuthorizedClientService authorizedClientService() {
+    return new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistrations());
   }
 }
